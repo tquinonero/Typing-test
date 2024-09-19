ed1247c88472b9e08c56084289d54207d8cd14a7
tquinonero <152458286+tquinonero@users.noreply.github.com>
2024-09-19 23:00:01 +0200

    Update typing-test.php
    Add more levels

--- typing-test.php
+++ typing-test.php
@@ -3,7 +3,7 @@
  * Plugin Name: Typing Test for WordPress
  * Plugin URI: https://innov8ion.tech
  * Description: A plugin to test your typing accuracy and speed
- * Version: 1.1
+ * Version: 1.2
  * Author: Innov8ion.tech
  * Author URI: https://innov8ion.tech
  * License: The Unlicense
@@ -23,18 +23,25 @@ class Typing_Test_WP {
 
     public function enqueue_scripts() {
         wp_enqueue_style('typing-test-style', plugins_url('assets/css/style.css', __FILE__));
-        wp_enqueue_script('typing-test-script', plugins_url('assets/js/typing-test-script.js', __FILE__), array('jquery'), '1.0', true);
+        wp_enqueue_script('typing-test-script', plugins_url('assets/js/typing-test-script.js', __FILE__), array('jquery'), '1.1', true);
+        // Remove this line if it was added:
+        // wp_enqueue_script('typing-test-auto', plugins_url('assets/js/typing-test-auto.js', __FILE__), array('jquery', 'typing-test-script'), '1.0', true);
     }
 
     public function typing_test_shortcode() {
         ob_start();
         ?>
         <div id="typing-test-container">
+            <div id="level-indicator">Level: <span id="current-level">1</span>/3</div>
             <div id="test-text"></div>
             <textarea id="user-input" rows="5" disabled></textarea>
             <div id="results"></div>
             <button id="start-test">Start Test</button>
             <button id="stop-test" disabled>Stop Test</button>
+            <button id="next-level" style="display:none;">Next Level</button>
+            <button id="reset-test" style="display:none;">Reset Test</button>
+            <!-- Remove this line if it was added: -->
+            <!-- <button id="auto-test">Auto Test</button> -->
         </div>
         <?php
         return ob_get_clean();
